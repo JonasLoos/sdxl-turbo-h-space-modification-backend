6479011c8fc181373f17ee94e8449b6707ceee52
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2024-01-23 18:26:34 +0100

    update code based on nextjs template

---
+++ app/predictions/route.js
@@ -0,0 +1,22 @@
+export const dynamic = 'force-dynamic' // defaults to auto
+import Replicate from "replicate";
+
+
+const replicate = new Replicate({auth: process.env.REPLICATE_API_TOKEN,});
+const model_owner = 'jonasloos';
+const model_name = 'sdxl-turbo-h-space-modification-model'
+let model_version = null;
+
+
+export async function GET(request) {
+    if (model_version === null) {
+        const model_versions = await replicate.models.versions.list(model_owner, model_name);
+        model_version = model_versions.results[0].id;
+    }
+    const output = await replicate.run(`${model_owner}/${model_name}:${model_version}`, {input: req.body});
+    return new Response(output, {
+        headers: {
+            'content-type': 'application/json',
+        },
+    })
+}
--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <div>
-            <h1>Nothing here...</h1>
-        </div>
-    );
-}
-  
