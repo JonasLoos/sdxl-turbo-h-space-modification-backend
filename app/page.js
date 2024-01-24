f5ddc8aabc2c3116d807092c0fc4734cc39e6b55
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2024-01-24 14:42:33 +0100

    add "no base prompt" option

--- app/page.js
+++ app/page.js
@@ -17,13 +17,11 @@ export default function Home() {
 
   const handleInputChange = (setter) => (e) => {
     setter(e.target.value);
-    if (timer) clearTimeout(timer);
-    setTimer(setTimeout(callApi, 500));
   };
 
   const callApi = () => {
     setActiveRequests(prev => prev + 1);
-    const prompt = attributes[basePrompt]['base_prompt'] + ', ' + extraPrompt;
+    const prompt = basePrompt < attributes.length ? attributes[basePrompt]['base_prompt'] + ' ' + extraPrompt : extraPrompt;
     console.log('Calling API with:', { prompt, scales, seed });
     const queryParams = new URLSearchParams({
       prompt: prompt,
@@ -49,8 +47,9 @@ export default function Home() {
   };
 
   useEffect(() => {
-    callApi();
-  }, []);
+    if (timer) clearTimeout(timer);
+    setTimer(setTimeout(callApi, 500));
+  }, [basePrompt, extraPrompt, scales, seed]);
 
   return (
     <div className="container">
@@ -64,10 +63,11 @@ export default function Home() {
       <div className="prompt-container">
         <div>
           <label htmlFor="seed">Base Prompt</label>
-          <select id='base-prompt' value={basePrompt} onChange={handleInputChange(setBasePrompt)}>
+          <select id='base-prompt' value={basePrompt} onChange={handleInputChange(x => {setBasePrompt(x); setScales(Array(num_attr).fill(0))})}>
             {attributes.map((attribute, i) => (
               <option key={i} value={i}>{attribute['base_prompt']}</option>
             ))}
+            <option value={attributes.length}>No Base Prompt</option>
           </select>
         </div>
         <div>
@@ -82,24 +82,33 @@ export default function Home() {
           />
         </div>
       </div>
-      <div className="sliders">
-        {attributes[basePrompt].attributes.map(({ attribute, prompt }, i) => {
-          const index = attributes.reduce((a, b, j) => j < basePrompt ? a + b.attributes.length : a, 0) + i;
-          return (
-            <div className="slider" key={attribute} title={prompt}>
-              <label htmlFor={`value-${attribute}`}>{attribute}</label>
-              <input
-                id={`value-${attribute}`}
-                type="range"
-                min="-5"
-                max="5"
-                value={scales[index]}
-                onChange={handleInputChange((x) => {setScales(prev => {prev[index] = x; return [...prev]})})}
-              />
-            </div>
-          )
-        })}
-      </div>
+      <div className='sliders'>{(() => {
+        const sliders = [];
+        let i = 0;
+        for (let j = 0; j < attributes.length; j++) {
+          const { base_prompt, attributes: inner_attributes } = attributes[j];
+          for (const { attribute, prompt } of inner_attributes) {
+            if (basePrompt == attributes.length || basePrompt == j) {
+              sliders.push(
+                <div className="slider" key={i} title={prompt}>
+                  <label htmlFor={`value-${attribute}`}>{(basePrompt == attributes.length ? base_prompt + ' - ' : '') + attribute}</label>
+                  <input
+                    id={`value-${attribute}`}
+                    type="range"
+                    min="-8"
+                    max="8"
+                    value={scales[i]}
+                    onChange={((i) => handleInputChange((x) => {setScales(prev => {const tmp = [...prev]; tmp[i] = parseInt(x); return tmp})}))(i)}
+                  />
+                </div>
+              );
+            }
+            i++;
+          }
+        }
+        console.log(scales);
+        return sliders;
+      })()}</div>
       <div id="description">
         <Description/>
       </div>
