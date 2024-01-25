6d810d45b19dfb6f0e9e8f6b4e3216f5eaeece42
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2024-01-25 16:52:32 +0100

    add last-updated line at the end

--- README.md
+++ README.md
@@ -3,3 +3,18 @@ Webapp for visualizing h-space modification of SDXL-Turbo.
 Deployed at [https://sdxl-turbo-h-space-modification.vercel.app/](https://sdxl-turbo-h-space-modification.vercel.app/) using [Vercel](https://vercel.com/).
 
 The model repo can be found [here](https://github.com/JonasLoos/sdxl-turbo-h-space-modification-model).
+
+## Development
+
+Run locally:
+
+```bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:3000
+npm run dev
+
+# run optimized build for production
+npm run build && npm run start
+```
--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,16 @@ import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
 import rehypeHighlight from 'rehype-highlight'
 import createMDX from '@next/mdx'
+import { execSync } from 'child_process';
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions`` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
+  // Pass the last commit date to the app for last-updated
+  env:{
+    LAST_COMMIT_DATE : execSync('git log -1 --format=%cd --date=short').toString().trim(),
+  }
 }
  
 const withMDX = createMDX({
